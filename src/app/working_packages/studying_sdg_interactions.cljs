3dc03666e0c77f6dfc22db44ecdb600bc83958bb
Mario Angst <mario.angst@gmail.com>
2022-05-02 14:23:49 +0200

    Update studying_sdg_interactions.cljs

--- src/app/working_packages/studying_sdg_interactions.cljs
+++ src/app/working_packages/studying_sdg_interactions.cljs
@@ -9,50 +9,23 @@
      work on charting the other side of complexity in urban sustainability
      transformations: the fundamental interconnectedness of SDG implementation
      challenges.  SDGs interact with each other in obvious and non-obvious
-     ways (Bennich et al. 2020). SDG interactions can be synergistic or
+     ways. SDG interactions can be synergistic or
      conflicting, meaning that goal achievement in one SDG (or target) can both
      positively or negatively affect goal achievement in another SDG (or
-     target) (Pham-Truffert et al. 2020).  We use gamification to study mental
-     models of SDG interactions held by actors in Zürich, in collaboration with
-     the ZHdK Gamelab. We explore the ways in which gamification and serious
-     games can help actors understand their own mental models about dynamics in
-     urban sustainability transformations, as well as the ways in which it can
-     help them understand mental models of others. We organize research in this
-     sub-project around two guiding questions: RQ2.1: How can gamification and
-     serious games contribute to the study of localized SDG interactions?
-     RQ2.2: How can gamification and serious games contribute to social
-     learning about dynamics in urban sustainability transformations between
-     actors?  To answer these questions, we build on and contribute to a lively
-     literature on mental models and participatory modeling in sustainability
-     research and complex systems in general (Moon et al. 2019). Mental model
-     research and participatory modeling is a prime target for gamification and
-     serious games (Bakhanova et al. 2020).  For RQ2.1, we gather information
-     about perceived interactions among a fixed number of elements of urban
-     sustainability transformations (mainly SDG targets, see figure 1b) (Van
-     Den Broek 2018). We expose a sample of representatives of organisational
-     actors identified in P1 to an interactive, digital representation of
-     Zurich. As common in mental model research, actors are then prompted to
-     specify their perceptions of how different elements of urban
-     sustainability interact. For example, do they think increases in the
-     quality of green spaces would lead to visitor pressure in a neighborhood?
-     Where do they think reduced car traffic would impact businesses most?
-     Where would an increase in tools for citizen participation yield most
-     engagement?  At the most basic level, gamification makes it possible for
-     actors to get direct feedback about the effect of their model parameter
-     choices. More interestingly, gamification opens up unique possibilities to
-     expose actors to effects of competing mental models or explorations of
-     development scenarios, which we explore with the ZHdK Gamelab. As a data
-     gathering approach, this procedure also yields the data to construct a
-     crowd-sourced and localized meta-model of SDG interactions in Zurich,
-     based on the combined mental models of participating actors.  To answer
-     RQ2.2 we explore the transdisciplinary potential of gamification and
-     serious games to promote social learning and shared understandings (or
-     acceptance for difference) of dynamics in urban sustainability
-     transformations. We examine how mental models among actor groups
-     differ (for example between powerful and marginal actors). In areas of
-     contestation, we collaborate with the ZHdK Gamelab to promote social
-     learning and resolve potential conflicts among actors in digital serious
-     games (Haug et al. 2011)."
+     target).  We use gamification to study mental
+     models of SDG interactions held by actors in Zürich.
+      SD-Game will investigate local knowledge which is relevant to the Sustainable Development Goals through 
+      digital and non-digital game elements supporting participatory systems modeling.
+      With a case study in the city of Zurich, the project will dive into the central 
+      role that gamification can play to identify local knowledge regarding interactions 
+      among the Sustainable Development Goals (SDGs), i.e. how progress toward 
+      one target can foster or hinder progress on (an)other target(s).
+      After exploring the questions of definition and contextualization of so-called 
+      “SDG interactions”, the project will test different degrees of gamification
+      in participatory systems modeling interventions, using notably an existing modeling tool, 
+      as well as a game which will be specially designed for the research purpose. 
+      The project will also reflect on the learning outcomes of the game elements for 
+      the participating stakeholders."
       :description-timeline "We plan to achieve the main outputs of this
 work-package until early 2024, namely relating to the Participatory
 Modeling (2.1) and Social learning (2.2)."
